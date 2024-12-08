c5270e8106ff478989a0a82546083c54eebe6c42
21120460 <tanhung126612@gmail.com>
2024-12-08 19:43:51 +0700

    forward and redirect

---
+++ forward-redirect/src/main/java/tan/hung/ResultServlet.java
@@ -0,0 +1,21 @@
+package tan.hung;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/result")
+public class ResultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+        out.println("This is result page.");
+    }
+}
